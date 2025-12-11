c949aa24fee424666003d950a1bc5e1903f32e65
yashvikhunt <yashvikhunt17@gmail.com>
2025-12-11 00:08:35 -0500

    feat: Update next.config.ts with output and basePath settings

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: "/AthleticCore",
   images: {
+    unoptimized: true,
     domains: ["images.unsplash.com", "source.unsplash.com"],
   },
 };
